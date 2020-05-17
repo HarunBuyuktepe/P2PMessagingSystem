f9151f4760a95416ffed9a5a1c116e66c942156e
EAhmetYazici <44259306+EAhmetYazici@users.noreply.github.com>
2020-05-17 03:03:12 +0300

    Update & Arrangement

--- src/com/CSE4057/NewServer.java
+++ src/com/CSE4057/NewServer.java
@@ -1,24 +1,23 @@
 package com.CSE4057;
 
 import java.io.*;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.*;
+import java.util.*;
 import java.security.*;
-import java.util.Base64;
-import java.util.HashMap;
 
 import static com.CSE4057.NewServer.*;
 import static com.CSE4057.NewServer.getClientInfo;
 import static com.CSE4057.NewServer.getClientPortInfo;
 
-public class NewServer {
-
+public class NewServer
+{
     private static Key privateKeyOfServer = null;
     private static Key publicKeyOfServer = null;
     private static HashMap clientInfo = null;
     private static HashMap clientPortInfo = null;
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception
+    {
         // don't need to specify a hostname, it will be the current machine
         NewServer newServer = new NewServer();
 
@@ -29,56 +28,58 @@ public class NewServer {
         ServerSocket ss = new ServerSocket(8018);
         System.out.println("ServerSocket awaiting connections...");
 
-        while (true) {
+        while (true)
+        {
             Socket s = null;
 
             try {
-                s = ss.accept(); // blocking call, this will wait until a connection is attempted on this port.
+                s = ss.accept(); 	// blocking call, this will wait until a connection is attempted on this port.
                 System.out.println("A new client is connected : " + s.getPort());
 
                 System.out.println("Assigning new thread for this client");
-                ObjectInputStream objectInputStream=new ObjectInputStream(s.getInputStream());
+                ObjectInputStream objectInputStream = new ObjectInputStream(s.getInputStream());
                 ObjectOutputStream objectOutputStream = new ObjectOutputStream(s.getOutputStream());
-                // create a new thread object
-                Thread t = new ClientHandler(s,objectInputStream,objectOutputStream,newServer);
-
+                
+                // Create a new thread object
+                Thread t = new ClientHandler(s, objectInputStream, objectOutputStream, newServer);
                 t.start();
 
             } catch (Exception e) {
                 System.out.println("olmii");
             }
         }
-
     }
-    public static Key getPrivateKeyOfServer(){return privateKeyOfServer;}
-    public static Key getPublicKeyOfServer(){return publicKeyOfServer;}
-    public static void setPrivateKeyOfServer(Key k){privateKeyOfServer=k;}
-    public static void setPublicKeyOfServer(Key k){publicKeyOfServer=k;}
+    
+    public static Key getPublicKeyOfServer() { return publicKeyOfServer; }
+    public static void setPublicKeyOfServer(Key k) { publicKeyOfServer = k; }
+    
+    public static Key getPrivateKeyOfServer() { return privateKeyOfServer; }
+    public static void setPrivateKeyOfServer(Key k) { privateKeyOfServer = k; }
 
     public static void addToHash(String userNameOfClient, byte[] Certificate) {
-        getClientInfo().put(userNameOfClient,Certificate);
+        getClientInfo().put(userNameOfClient, Certificate);
     }
 
-    public static HashMap getClientInfo(){return clientInfo;}
+    public static HashMap getClientInfo() { return clientInfo; }
+    public static HashMap getClientPortInfo() { return clientPortInfo; }
 
-    public static HashMap getClientPortInfo(){return clientPortInfo;}
-
-    public static void generateKey() throws Exception {
-        // here we generate server keys
-        KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
+    public static void generateKey() throws Exception			// Generate Server Keys
+    {
+        KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");			// KeyPairGenerator with RSA Mode
         kpg.initialize(4096);
-        KeyPair kp = kpg.generateKeyPair();
-        System.out.println("Public Key : "+Base64.getEncoder().encodeToString(kp.getPublic().getEncoded()));
-        setPrivateKeyOfServer(kp.getPrivate());
-        setPublicKeyOfServer(kp.getPublic());
+        KeyPair kp = kpg.generateKeyPair();			// KeyPair for Public-Private Keys
+        System.out.println("Public Key : " + Base64.getEncoder().encodeToString(kp.getPublic().getEncoded()));
+        setPrivateKeyOfServer(kp.getPrivate());		// Private Key of Server
+        setPublicKeyOfServer(kp.getPublic());		// Public Key of Server
     }
+    
 }
 
 // ClientHandler class
 class ClientHandler extends Thread
 {
-//    final ObjectInputStream ois;
-//    final ObjectOutputStream oos;
+	// final ObjectInputStream ois;
+	// final ObjectOutputStream oos;
     final Socket s;
     // create a DataInputStream so we can read data from it.
     ObjectInputStream objectInputStream = null;
@@ -89,8 +90,10 @@ class ClientHandler extends Thread
     Boolean sendedCertificate = false;
     NewServer newServer = null;
     int portNumber = 0;
+    
     // Constructor
-    public ClientHandler(Socket s, ObjectInputStream objectInputStream, ObjectOutputStream objectOutputStream,NewServer newServer) {
+    public ClientHandler(Socket s, ObjectInputStream objectInputStream, ObjectOutputStream objectOutputStream, NewServer newServer)
+    {
         this.s = s;
         this.objectInputStream = objectInputStream;
         this.objectOutputStream = objectOutputStream;
@@ -101,21 +104,27 @@ class ClientHandler extends Thread
     public void run()
     {
         System.out.println(objectInputStream);
-        Object o=null;
+        Object o = null;
         try {
             objectOutputStream.writeObject(getPublicKeyOfServer());
         } catch (IOException e) {
             System.out.println("Sending public key of server");
         }
-        while (true){
+        
+        while (true)
+        {
             try {
                 o = (Object) objectInputStream.readObject();
+                
                 if (o instanceof Key) {
                     System.out.println("Key is brought");
                     publicKeyOfClient = (Key) o;
                     sendedCertificate = false;
                     System.out.println(publicKeyOfClient);
-                } else if (o instanceof String){
+                } 
+                
+                else if (o instanceof String)
+                {
                     System.out.println("String is brought");
                     String stringComing = (String) o;
                     if(stringComing.equals("verified certificate")){
@@ -131,58 +140,62 @@ class ClientHandler extends Thread
                         objectOutputStream.writeObject(newServer.getClientInfo());
                         objectOutputStream.writeObject(newServer.getClientPortInfo());
                         getClientInfo().forEach((key, value) -> {
-                            System.out.println(key+" "+Base64.getEncoder().encodeToString((byte[]) value));
+                            System.out.println(key + " " + Base64.getEncoder().encodeToString((byte[]) value));
                         });
                     }
                     else {
-                        if(userNameOfClient==null){
+                        if(userNameOfClient == null){
                             userNameOfClient = stringComing;
-                            System.out.println("User name of the current client :  "+userNameOfClient);
+                            System.out.println("User name of the current client :  " + userNameOfClient);
                         } else {
-
-                            objectOutputStream.writeObject(new String("wrong command is "+stringComing));
+                            objectOutputStream.writeObject(new String("wrong command is " + stringComing));
                         }
 
                     }
                     System.out.println(stringComing);
 
-                } else if(o instanceof Integer){
+                } 
+                
+                else if(o instanceof Integer) {
                     portNumber = (int) o ;
                     System.out.println(portNumber);
-                    savePort(userNameOfClient,portNumber);
+                    savePort(userNameOfClient, portNumber);
                     sendedCertificate = false;
                 }
-                if(!sendedCertificate && userNameOfClient != null && publicKeyOfClient != null && portNumber !=0){
+                
+                if(!sendedCertificate && userNameOfClient != null && publicKeyOfClient != null && portNumber != 0) {
                     System.out.println("sending certificate");
-                    certificate = certificate(publicKeyOfClient,getPublicKeyOfServer());
-                    System.out.println("Sertifika uzunluğu "+certificate.length);
+                    certificate = certificate(publicKeyOfClient, getPublicKeyOfServer());
+                    System.out.println("Length of Certificate : " + certificate.length);
                     objectOutputStream.writeObject(certificate);
                 }
 
             }catch (Exception e){
-//                getClientInfo().remove(userNameOfClient);
-//                getClientPortInfo().remove(userNameOfClient);
+            	// getClientInfo().remove(userNameOfClient);
+            	// getClientPortInfo().remove(userNameOfClient);
                 return;
             }
         }
 
-
     }
+    
     private void savePort(String userNameOfClient, int port) {
-        getClientPortInfo().put(userNameOfClient,port );
+        getClientPortInfo().put(userNameOfClient, port);
     }
+    
     private void saveCertificate(String userNameOfClient) throws Exception {
-//        bir yere save etmeli
-    //    System.out.println(Base64.getEncoder().encodeToString(publicKeyOfClient.getEncoded()));
+    	// bir yere save etmeli
+    	// System.out.println(Base64.getEncoder().encodeToString(publicKeyOfClient.getEncoded()));
         Crypt crypt = new Crypt();
-        byte[] cipherText = crypt.encrypt(publicKeyOfClient,getPrivateKeyOfServer());
-        newServer.addToHash(userNameOfClient,cipherText );
+        byte[] cipherText = crypt.encrypt(publicKeyOfClient, getPrivateKeyOfServer());
+        newServer.addToHash(userNameOfClient, cipherText);
     }
 
-    public byte[] certificate(Key publicKeyOfClient, Key privateKeyOfServer) throws Exception {
-        // here we sign public key of client with server private key
+    public byte[] certificate(Key publicKeyOfClient, Key privateKeyOfServer) throws Exception {		// Sign Pub. Key of Client with Private Key of Server
         Crypt crypt = new Crypt();
-        byte[] cipherText = crypt.encrypt(publicKeyOfClient,getPrivateKeyOfServer());
-        return cipherText; // return digital signature
+        byte[] cipherText = crypt.encrypt(publicKeyOfClient, getPrivateKeyOfServer());		// Ciphered Text with Keys of Client & Server
+        
+        return cipherText; 		// return Digital Signature
     }
+    
 }
